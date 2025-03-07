335f75c8d70886a24a98934b880e3e63b10041e4
David·Jam <lgy112112@gmail.com>
2025-03-07 23:19:15 +0800

    Bump version to 1.1.0 and update package metadata
    - Update package version from 0.2.0 to 1.1.0
    - Modify package name in setup.py from kan_family to ikan
    - Add scikit-learn to package requirements
    - Update distribution files and egg-info metadata
    - Clean up unnecessary cached files

--- ikan/__init__.py
+++ ikan/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "0.2.0"
+__version__ = "1.1.0"
 
 from .KAN import KANLinear, KAN
 from .ChebyKAN import ChebyKANLinear, ChebyKAN
--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from setuptools import setup, find_packages
 
 setup(
     name="ikan",  # 包名称，用作 pip install 的名字
-    version="0.2.0",  # 包版本
+    version="1.1.0",  # 包版本
     author="Guoying LIAO",  # 作者姓名
     author_email="lgy112112@gmail.com",  # 作者邮箱
     description="An efficient KAN implementation in Chinese.",  # 简要描述
     long_description=open("README.md", encoding="utf-8").read(),
     long_description_content_type="text/markdown",  # README 文件格式
     url="https://github.com/lgy112112/Efficient-KAN-in-Chinese",  # 仓库地址
-    packages=find_packages(include=["kan_family", "kan_family.*"]),  # 自动发现所有子包
+    packages=find_packages(include=["ikan", "ikan.*"]),  # 自动发现所有子包
     install_requires=[
         "torch>=1.9.0",
         "torchinfo",
